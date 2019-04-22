400123bbcf5288ee65ae82bd95f168ccbd7731d5
francescaberkoh <47398177+francescaberkoh@users.noreply.github.com>
2019-04-22 18:06:23 -0400

    Add files via upload

---
+++ 404.py
@@ -0,0 +1,36 @@
+'''
+Created on Apr 22, 2019
+
+@author: Francesca
+'''
+
+num = input("Enter your mark here:")
+
+if num =="4+":
+    print("You got a 98" )
+elif num =="4":
+    print("You got a 90" )
+elif num =="4-":
+    print("You got a 83" )
+elif num =="3+":
+    print("You got a 78" )
+elif num =="3":
+    print("You got a 74.5" )
+elif num =="3-":
+    print("You got a 71" )
+elif num =="2+":
+    print("You got a 68" )
+elif num =="2":
+    print("You got a 64" )
+elif num =="2-":
+    print("You got a 61" )
+elif num =="1+":
+    print("You got a 58" )
+elif num =="1":
+    print("You got a 55" )
+elif num =="1-":
+    print("You got a 51" )
+elif num =="0":
+    print("You got a 49 or less" )
+else:
+    print("ERROR! place a mark between 0-4+")
